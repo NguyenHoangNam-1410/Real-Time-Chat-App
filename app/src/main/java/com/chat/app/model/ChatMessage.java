12180978a27a4a0be99756b0aa9a832f385fb398
silver soul <silversoul1410@gmail.com>
2025-04-15 12:09:19 +0700

    Version 1.0

---
+++ app/src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.chat.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private Long Id;
+    private String sender;
+    private String message;
+
+}
