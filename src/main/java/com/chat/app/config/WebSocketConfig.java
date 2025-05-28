b1cf5c104011105afe383c75004f945e3c12adc9
silver soul <silversoul1410@gmail.com>
2025-05-28 09:23:04 +0700

    Version 2.1 - Add mp3 sending function

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -5,21 +5,32 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
-
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-        .setAllowedOrigins("http://localhost:8080").withSockJS();
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS()
+                .setStreamBytesLimit(512 * 1024) // 512KB
+                .setHttpMessageCacheSize(1000)
+                .setDisconnectDelay(30 * 1000);
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        //set message broker to use a simple in-memory broker
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+        registry.setMessageSizeLimit(512 * 1024); // 512KB
+        registry.setSendBufferSizeLimit(1024 * 1024); // 1MB
+        registry.setSendTimeLimit(20000); // 20 seconds
     }
 }
